0b603567b9d50dfe282027def55bb72acee0b251
intendo <intendo23@gmail.com>
2020-07-03 15:44:23 -0700

    Ran pylint on setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
 
 """
-The use of continuum solvation methods such as APBS requires accurate and complete
-structural data as well as force field parameters such as atomic charges and radii.
-Unfortunately, the limiting step in continuum electrostatics calculations is often
-the addition of missing atomic coordinates to molecular structures from the Protein
-Data Bank and the assignment of parameters to these structures. To adds this problem,
-we have developed PDB2PQR. This software automates many of the common tasks of
-preparing structures for continuum solvation calculations as well as many other types
-of biomolecular structure modeling, analysis, and simulation. These tasks include:
+The use of continuum solvation methods such as APBS requires accurate and
+complete structural data as well as force field parameters such as atomic
+charges and radii. Unfortunately, the limiting step in continuum electrostatics
+calculations is often the addition of missing atomic coordinates to molecular
+structures from the Protein Data Bank and the assignment of parameters to these
+structures. To adds this problem, we have developed PDB2PQR. This software
+automates many of the common tasks of preparing structures for continuum
+solvation calculations as well as many other types of biomolecular structure
+modeling, analysis, and simulation. These tasks include:
 
-* Adding a limited number of missing heavy (non-hydrogen) atoms to biomolecular structures.
-* Estimating titration states and protonating biomolecules in a manner consistent with favorable hydrogen bonding.
+* Adding a limited number of missing heavy (non-hydrogen) atoms to biomolecular
+  structures.
+* Estimating titration states and protonating biomolecules in a manner
+  consistent with favorable hydrogen bonding.
 * Assigning charge and radius parameters from a variety of force fields.
-* Generating "PQR" output compatible with several popular computational modeling and analysis packages.
+* Generating "PQR" output compatible with several popular computational modeling
+  and analysis packages.
 
-This service is intended to facilitate the setup and execution of electrostatics calculations for both experts and non-experts and thereby broaden the accessibility of
-biomolecular solvation and electrostatics analyses to the biomedical community.
+This service is intended to facilitate the setup and execution of electrostatics
+calculations for both experts and non-experts and thereby broaden the
+accessibility of biomolecular solvation and electrostatics analyses to the
+biomedical community.
 """
 
 import sys
@@ -26,7 +32,7 @@ if sys.version_info[:2] < (3, 6):
     raise RuntimeError("Python version >= 3.6 is required.")
 
 with open("README.md", "r") as f:
-    long_description = f.read()
+    LONG_DESCRIPTION = f.read()
 
 MAJOR = 3
 MINOR = 0
@@ -42,7 +48,7 @@ setuptools.setup(
         "Automates many of the common tasks of preparing structures for "
         "continuum solvation calculations as well as many other types of "
         "biomolecular structure modeling, analysis, and simulation."),
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     install_requires=["propka >= 3.2", "pandas >= 1.0", "pytest>=5.4.1"],
     url="http://www.poissonboltzmann.org",
     packages=setuptools.find_packages(

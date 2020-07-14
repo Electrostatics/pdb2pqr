2ade0c01a0c28bf6284eb6be46b6a9292f58b235
intendo <intendo23@gmail.com>
2020-07-14 14:27:57 -0700

    Fixed long lines

--- tests/logging_test.py
+++ tests/logging_test.py
@@ -7,7 +7,8 @@ import common
 @log_capture()
 @pytest.mark.parametrize(
     "input_file,output_file",
-    [('1FAS.pdb', '1FAS_pdb.pqr'), ('1A1P.pdb', '1A1P_assign-only_whitespace_ff=AMBER.pqr')]
+    [('1FAS.pdb', '1FAS_pdb.pqr'),
+     ('1A1P.pdb', '1A1P_assign-only_whitespace_ff=AMBER.pqr')]
 )
 def test_log_output_in_pqr_location(capture, input_file, output_file):
     """Test that a log file is generated when calling the __init__.py"""
@@ -22,7 +23,9 @@ def test_log_output_in_pqr_location(capture, input_file, output_file):
         tmp_path=tmp_path
     )
 
-    # This match tests that the log file is output to the same location as the output pqr
+    # This match tests that the log file is output to the same location
+    # as the output pqr
     capture.check_present(
-        ('pdb2pqr.input_output', 'INFO', f'Logs stored: tests/data/{output_file.split(".")[0]}.log')
+        ('pdb2pqr.input_output', 'INFO',
+         f'Logs stored: tests/data/{output_file.split(".")[0]}.log')
     )

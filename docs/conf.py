4b9afb4f6bb87284d1a9341ced852f548267be46
Nathan Baker <nathanandrewbaker@gmail.com>
2020-07-17 12:18:29 -0700

    Troubleshooting RTD.

--- docs/conf.py
+++ docs/conf.py
@@ -45,6 +45,7 @@ autosummary_generate_overwrite = True
 
 # Add any paths that contain templates here, relative to this directory.
 # templates_path = ['_templates']
+master_doc = 'index'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.

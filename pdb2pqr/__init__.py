bf19dbabb2e7e2afe4536827e302fee2a298f4cf
intendo <intendo23@gmail.com>
2020-09-07 17:06:11 -0700

    Closes #55 and #87

--- pdb2pqr/__init__.py
+++ pdb2pqr/__init__.py
@@ -8,13 +8,15 @@ For more information, see http://www.poissonboltzmann.org/
 import logging
 from sys import version_info
 from .main import main_driver, build_main_parser
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
 
 assert version_info >= (3, 5)
 
-
 _LOGGER = logging.getLogger(__name__)
 logging.captureWarnings(True)
-
 
 if __name__ == "__main__":
     PARSER = build_main_parser()

5935302094e7db93634d3bec2b3074cc3a976e2d
intendo <intendo23@gmail.com>
2020-10-06 13:36:00 -0700

    Revert "Add inputgen.py"
    This reverts commit a97bf415eb6276eda8b19a6cf3e3b01b06ceafd8.

--- pdb2pqr/inputgen.py
+++ pdb2pqr/inputgen.py
@@ -94,54 +94,79 @@ class Elec:
             self.write = [["pot", "dx", "pot"]]
 
     def __str__(self):
-        text = f"elec {self.label}\n"
-        text += f"    {self.method}\n"
-        text += f"    dime {self.dime[0]} {self.dime[1]} {self.dime[2]}\n"
+        text = "elec %s\n" % self.label
+        text += "    %s\n" % self.method
+        text += "    dime %i %i %i\n" % (
+            self.dime[0],
+            self.dime[1],
+            self.dime[2],
+        )
         if self.method == "mg-manual":
-            text += f"    glen {self.glen[0]:.3f} {self.glen[1]:.3f} "
-            text += f"{self.glen[2]:.3f}\n"
-            text += f"    gcent {self.gcent}\n"
+            text += "    glen %.3f %.3f %.3f\n" % (
+                self.glen[0],
+                self.glen[1],
+                self.glen[2],
+            )
+            text += "    gcent %s\n" % self.gcent
         elif self.method == "mg-auto":
-            text += f"    cglen {self.cglen[0]:.4f} {self.cglen[1]:.4f} "
-            text += f"{self.cglen[2]:.4f}\n"
-            text += f"    fglen {self.fglen[0]:.4f} {self.fglen[1]:.4f} "
-            text += f"{self.fglen[2]:.4f}\n"
-            text += f"    cgcent {self.cgcent}\n"
-            text += f"    fgcent {self.fgcent}\n"
+            text += "    cglen %.4f %.4f %.4f\n" % (
+                self.cglen[0],
+                self.cglen[1],
+                self.cglen[2],
+            )
+            text += "    fglen %.4f %.4f %.4f\n" % (
+                self.fglen[0],
+                self.fglen[1],
+                self.fglen[2],
+            )
+            text += "    cgcent %s\n" % self.cgcent
+            text += "    fgcent %s\n" % self.fgcent
         elif self.method == "mg-para":
-            text += f"    pdime {self.pdime[0]} {self.pdime[1]} "
-            text += f"{self.pdime[2]}\n"
-            text += f"    ofrac {self.ofrac:.1f}\n"
-            text += f"    cglen {self.cglen[0]:.4f} {self.cglen[1]:.4f} "
-            text += f"{self.cglen[2]:.4f}\n"
-            text += f"    fglen {self.fglen[0]:.4f} {self.fglen[1]:.4f} "
-            text += f"{self.fglen[2]:.4f}\n"
-            text += f"    cgcent {self.cgcent}\n"
-            text += f"    fgcent {self.fgcent}\n"
+            text += "    pdime %i %i %i\n" % (
+                self.pdime[0],
+                self.pdime[1],
+                self.pdime[2],
+            )
+            text += "    ofrac %.1f\n" % self.ofrac
+            text += "    cglen %.4f %.4f %.4f\n" % (
+                self.cglen[0],
+                self.cglen[1],
+                self.cglen[2],
+            )
+            text += "    fglen %.4f %.4f %.4f\n" % (
+                self.fglen[0],
+                self.fglen[1],
+                self.fglen[2],
+            )
+            text += "    cgcent %s\n" % self.cgcent
+            text += "    fgcent %s\n" % self.fgcent
             if self.asyncflag:
-                text += f"    async {self.async_}\n"
-        text += f"    mol {self.mol}\n"
+                text += "    async %i\n" % self.async_
+        text += "    mol %i\n" % self.mol
         if self.lpbe:
             text += "    lpbe\n"
         else:
             text += "    npbe\n"
-        text += f"    bcfl {self.bcfl}\n"
+        text += "    bcfl %s\n" % self.bcfl
         if self.istrng > 0:
             for ion in self.ion:
-                text += f"    ion charge {ion[0]:.2f} conc {self.istrng:.3f} "
-                text += f"radius {ion[0]:.4f}\n"
-        text += f"    pdie {self.pdie:.4f}\n"
-        text += f"    sdie {self.sdie:.4f}\n"
-        text += f"    srfm {self.srfm}\n"
-        text += f"    chgm {self.chgm}\n"
-        text += f"    sdens {self.sdens:.2f}\n"
-        text += f"    srad {self.srad:.2f}\n"
-        text += f"    swin {self.swim:.2f}\n"
-        text += f"    temp {self.temp:.2f}\n"
-        text += f"    calcenergy {self.calcenergy}\n"
-        text += f"    calcforce {self.calcforce}\n"
+                text += "    ion charge %.2f conc %.3f radius %.4f\n" % (
+                    ion[0],
+                    self.istrng,
+                    ion[1],
+                )
+        text += "    pdie %.4f\n" % self.pdie
+        text += "    sdie %.4f\n" % self.sdie
+        text += "    srfm %s\n" % self.srfm
+        text += "    chgm %s\n" % self.chgm
+        text += "    sdens %.2f\n" % self.sdens
+        text += "    srad %.2f\n" % self.srad
+        text += "    swin %.2f\n" % self.swin
+        text += "    temp %.2f\n" % self.temp
+        text += "    calcenergy %s\n" % self.calcenergy
+        text += "    calcforce %s\n" % self.calcforce
         for write in self.write:
-            text += f"    write {write[0]} {write[1]} {write[2]}\n"
+            text += "    write %s %s %s\n" % (write[0], write[1], write[2])
         text += "end\n"
         return text
 
@@ -197,7 +222,7 @@ class Input:
 
     def __str__(self):
         text = "read\n"
-        text += f"    mol pqr {self.pqrname}\n"
+        text += "    mol pqr %s\n" % self.pqrname
         text += "end\n"
         for elec in self.elecs:
             text += str(elec)
@@ -225,7 +250,7 @@ class Input:
             elec = self.elecs[0]
             nproc = elec.pdime[0] * elec.pdime[1] * elec.pdime[2]
             for i in range(int(nproc)):
-                outname = base_name + f"-PE{i}.in"
+                outname = base_name + "-PE%i.in" % i
                 for elec in self.elecs:
                     elec.asyncflag = True
                     elec.async_ = i
@@ -266,15 +291,15 @@ def split_input(filename):
                 words = line.split()
                 nproc = int(words[1]) * int(words[2]) * int(words[3])
     if nproc == 0:
-        errstr = f"{filename} is not a valid APBS parallel input file!\n"
+        errstr = "%s is not a valid APBS parallel input file!\n" % filename
         errstr = errstr + (
             "The inputgen script was unable to asynchronize this file!"
         )
         raise RuntimeError(errstr)
     base_pqr_name = Path(filename).stem
     for iproc in range(nproc):
-        outname = base_pqr_name + f"-PE{iproc}.in"
-        outtext = text.replace("mg-para\n", f"mg-para\n    async {iproc}\n")
+        outname = base_pqr_name + "-PE%i.in" % iproc
+        outtext = text.replace("mg-para\n", "mg-para\n    async %i\n" % iproc)
         outfile = open(outname, "w")
         outfile.write(outtext)
         outfile.close()

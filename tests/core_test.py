0107983d8c1f395288bb335002aad57f9e97a19c
Nathan Baker <nathanandrewbaker@gmail.com>
2021-12-31 09:31:37 -0800

    Balance PDB input coverage in tests.
    This commit removes some of the original input files for the "short"
    tests and adds new ones that ensure coverage across DNA, RNA, proteins,
    and protein-nucleic complexes.  It also runs the same tests for both
    PDB and CIF formats.

--- tests/core_test.py
+++ tests/core_test.py
@@ -11,15 +11,19 @@ _LOGGER = logging.getLogger(__name__)
 #: Protein-nucleic acid complexes
 PROTEIN_NUCLEIC_SET = {"4UN3"}
 #: Proteins without nucleic acids
-PROTEIN_ONLY_SET = {"1K1I", "1AFS", "1FAS", "5DV8", "5D8V", "1E7G"}
+PROTEIN_ONLY_SET = {"1K1I", "1FAS", "5D8V"}
 #: DNA without protein
 DNA_ONLY_SET = {"1NAJ", "7BNA"}
 #: RNA without protein
-RNA_ONLY_SET = {"5V0O", "1AQO", "4E8M"}
+RNA_ONLY_SET = {"1AQO", "4E8M"}
 #: Nucleic acids without proteins
 NUCLEIC_ONLY_SET = DNA_ONLY_SET | RNA_ONLY_SET
 #: Basic test set
-SHORT_SET = {"4UN3", "1K1I", "1AFS", "1FAS", "5DV8", "5D8V", "1E7G"}
+SHORT_SET = PROTEIN_NUCLEIC_SET | PROTEIN_ONLY_SET | NUCLEIC_ONLY_SET
+#: Long test set, primarily protein-based
+LONG_PROTEIN_SET = {"5DV8", "1AFS", "1E7G"}
+LONG_NUCLEIC_SET = {"5V0O"}
+LONG_SET = LONG_PROTEIN_SET | LONG_NUCLEIC_SET
 #: Tests that should fail (broken backbones)
 BROKEN_SET = {"1EJG", "3U7T"}
 
@@ -32,8 +36,8 @@ _LOGGER.warning(
 
 
 @pytest.mark.parametrize("input_pdb", list(SHORT_SET), ids=str)
-def test_basic_pdb(input_pdb, tmp_path):
-    """Non-regression tests on PDB-format biomolecules without ligands."""
+def test_short_pdb(input_pdb, tmp_path):
+    """Non-regression tests on short list of PDB-format biomolecules."""
     args = "--log-level=INFO --ff=AMBER --drop-water --apbs-input=apbs.in"
     output_pqr = Path(input_pdb).stem + ".pqr"
     common.run_pdb2pqr(
@@ -44,26 +48,11 @@ def test_basic_pdb(input_pdb, tmp_path):
     )
 
 
-# TODO - replace this with SHORT_SET
-@pytest.mark.parametrize("input_pdb", ["1FAS"], ids=str)
+@pytest.mark.parametrize("input_pdb", list(SHORT_SET), ids=str)
 def test_basic_cif(input_pdb, tmp_path):
-    """Non-regression tests on CIF-format biomolecules without ligands."""
+    """Non-regression tests on short list of CIF-format biomolecules."""
     args = "--log-level=INFO --ff=AMBER --drop-water --apbs-input=apbs.in"
     output_pqr = Path(input_pdb).stem + ".cif"
-    common.run_pdb2pqr(
-        args=args,
-        input_pdb=input_pdb,
-        output_pqr=output_pqr,
-        tmp_path=tmp_path,
-    )
-
-
-# TODO - reduce redundancy in code
-@pytest.mark.parametrize("input_pdb", list(NUCLEIC_ONLY_SET), ids=str)
-def test_nucleic_only(input_pdb, tmp_path):
-    """Non-regression tests on structures that contain only nucleic acids."""
-    args = "--log-level=INFO --ff=AMBER --drop-water --apbs-input=apbs.in"
-    output_pqr = Path(input_pdb).stem + ".pqr"
     common.run_pdb2pqr(
         args=args,
         input_pdb=input_pdb,

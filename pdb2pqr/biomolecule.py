26b0a2fc432029f451b7e9e3516940e929acff13
Stefan Doerr <stefdoerr@gmail.com>
2022-02-22 14:41:28 +0200

    added comments

--- pdb2pqr/biomolecule.py
+++ pdb2pqr/biomolecule.py
@@ -749,8 +749,13 @@ class Biomolecule(object):
                             rname != residue.name
                         ):
                             if len(rname) == 4 and rname[0] in ("C", "N"):
+                                # Remove the C/N prefix to keep the protonation state of the residue
+                                # in the terminal residues
                                 rname = rname[1:]
                             else:
+                                # This is the old code which will overwrite the protonation state
+                                # of the residue but have wrong hydrogens. Not sure if needed but
+                                # nobody seems to know.
                                 rname = residue.name
                     except AttributeError:
                         pass

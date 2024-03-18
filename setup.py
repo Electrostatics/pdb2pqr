37b3b78204c1eb17eeb63ed5430ac06533d373c5
Skanda <sastrys1@gene.com>
2024-03-18 12:42:53 -0700

    added needed packages

--- setup.py
+++ setup.py
@@ -29,9 +29,9 @@ from sys import version_info
 from setuptools import find_packages, setup
 
 # NOTE: The following reads the version number and makes
-#       if available to the packaging tools before installation.
-#       REF: https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package  # noqa: E501
-#       This makes __version__ valid below
+#	if available to the packaging tools before installation.
+#	REF: https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package  # noqa: E501
+#	This makes __version__ valid below
 with open("pdb2pqr/_version.py") as fobj:
     exec(fobj.read())
 
@@ -47,75 +47,81 @@ setup(
     author="Jens Erik Nielsen, Nathan Baker, and many others.",
     author_email="nathanandrewbaker@gmail.com",
     description=(
-        "Automates many of the common tasks of preparing structures for "
-        "continuum solvation calculations as well as many other types of "
-        "biomolecular structure modeling, analysis, and simulation."
+	"Automates many of the common tasks of preparing structures for "
+	"continuum solvation calculations as well as many other types of "
+	"biomolecular structure modeling, analysis, and simulation."
     ),
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     install_requires=[
-        "mmcif_pdbx>=1.1.2",
-        "numpy",
-        "propka >= 3.5",
-        "requests",
-        "docutils < 0.18",
+	"mmcif_pdbx>=1.1.2",
+	"numpy",
+	"propka >= 3.5",
+	"requests",
+	"docutils < 0.18",
+	"torch",
+	"torchani = 2.2.0",
+	"ase",
+	"joblib",
+	"ambertools",
+	"scipy" 
     ],
     url="http://www.poissonboltzmann.org",
     packages=find_packages(
-        exclude=["pdb2pka", "*.pdb2pka", "pdb2pka.*", "*.pdb2pka.*"]
+	exclude=["pdb2pka", "*.pdb2pka", "pdb2pka.*", "*.pdb2pka.*"]
     ),
     package_data={
-        "pdb2pqr": ["dat/*.xml", "dat/*.DAT", "dat/*.names", "dat/*.joblib"]
+	"pdb2pqr": ["dat/*.xml", "dat/*.DAT", "dat/*.names", "dat/*.joblib"]
     },
     python_requires=">=3.5",
     extras_require={
-        "dev": ["check-manifest"],
-        "test": [
-            "black",
-            "coverage",
-            "flake8",
-            "pandas >= 1.0",
-            "pytest",
-            "testfixtures",
-        ],
+	"dev": ["check-manifest"],
+	"test": [
+	    "black",
+	    "coverage",
+	    "flake8",
+	    "pandas >= 1.0",
+	    "pytest",
+	    "testfixtures",
+	],
     },
     tests_require=[
-        "pandas >= 1.0",
-        "pytest",
-        "testfixtures",
+	"pandas >= 1.0",
+	"pytest",
+	"testfixtures",
     ],
     test_suite="tests",
     license="BSD",
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Environment :: Console",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: BSD License",
-        "Natural Language :: English",
-        "Operating System :: MacOS",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Topic :: Scientific/Engineering :: Bio-Informatics",
-        "Topic :: Scientific/Engineering :: Chemistry",
+	"Development Status :: 4 - Beta",
+	"Environment :: Console",
+	"Intended Audience :: Science/Research",
+	"License :: OSI Approved :: BSD License",
+	"Natural Language :: English",
+	"Operating System :: MacOS",
+	"Operating System :: Microsoft :: Windows",
+	"Operating System :: POSIX",
+	"Programming Language :: Python :: 3.8",
+	"Programming Language :: Python :: 3.9",
+	"Programming Language :: Python :: 3.10",
+	"Programming Language :: Python :: 3.11",
+	"Topic :: Scientific/Engineering :: Bio-Informatics",
+	"Topic :: Scientific/Engineering :: Chemistry",
     ],
     project_urls={
-        "Documentation": "https://pdb2pqr.readthedocs.io/",
-        "Get help": "https://github.com/Electrostatics/pdb2pqr/issues",
-        "Publications": "https://pubmed.ncbi.nlm.nih.gov/?term=R01+GM069702",
-        "Funding": "https://bit.ly/apbs-funding",
-        "Source": "https://github.com/Electrostatics/pdb2pqr",
+	"Documentation": "https://pdb2pqr.readthedocs.io/",
+	"Get help": "https://github.com/Electrostatics/pdb2pqr/issues",
+	"Publications": "https://pubmed.ncbi.nlm.nih.gov/?term=R01+GM069702",
+	"Funding": "https://bit.ly/apbs-funding",
+	"Source": "https://github.com/Electrostatics/pdb2pqr",
     },
     keywords="science chemistry molecular biology",
     entry_points={
-        "console_scripts": [
-            "pdb2pqr=pdb2pqr.main:main",
-            "pdb2pqr30=pdb2pqr.main:main",
-            "dx2cube=pdb2pqr.main:dx_to_cube",
-            "inputgen=pdb2pqr.inputgen:main",
-        ]
+	"console_scripts": [
+	    "pdb2pqr=pdb2pqr.main:main",
+	    "pdb2pqr30=pdb2pqr.main:main",
+	    "dx2cube=pdb2pqr.main:dx_to_cube",
+	    "inputgen=pdb2pqr.inputgen:main",
+	]
     },
 )

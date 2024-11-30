2cec21db511f372ee537b5c75e789acb30107ef7
Kiyoon Kim <kiyoon@users.noreply.github.com>
2024-12-01 01:01:45 +0900

    fix: add setup_logging in test

--- tests/common.py
+++ tests/common.py
@@ -212,6 +212,8 @@ def run_pdb2pqr_for_tests(
     compare_resnames=False,
 ):
     """Basic code for invoking PDB2PQR."""
+    from pdb2pqr import io
+
     if output_pqr is None:
         hash_str = f"{args}{input_pdb}"
         hash_ = hashlib.sha1(hash_str.encode("UTF-8")).hexdigest()
@@ -220,6 +222,7 @@ def run_pdb2pqr_for_tests(
     _LOGGER.debug(f"Writing output to {output_pqr}")
     arg_str = f"{args} {input_pdb} {output_pqr}"
     args = PARSER.parse_args(arg_str.split())
+    io.setup_logger(args.output_pqr, args.log_level)
     _ = main_driver(args)
     if expected_pqr is not None:
         compare_pqr(

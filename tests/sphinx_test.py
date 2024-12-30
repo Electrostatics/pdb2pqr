df8deb2f93e81d277918eee3345efe03d4770d79
Nathan Baker <1933272+sobolevnrm@users.noreply.github.com>
2024-12-30 03:12:13 +0000

    Add test for Sphinx documentation build.

---
+++ tests/sphinx_test.py
@@ -0,0 +1,42 @@
+"""Test Sphinx compilation of documentation."""
+
+from sphinx.application import Sphinx
+from pathlib import Path
+
+
+SOURCE_DIR = Path("docs/source")
+CONFIG_DIR = SOURCE_DIR
+
+
+def test_html_documentation(tmp_path):
+    """Test Sphinx build of HTML documentation."""
+    source_dir = SOURCE_DIR
+    config_dir = CONFIG_DIR
+    output_dir = tmp_path
+    doctree_dir = tmp_path / "doctrees"
+    app = Sphinx(
+        source_dir,
+        config_dir,
+        output_dir,
+        doctree_dir,
+        buildername="html",
+        warningiserror=True,
+    )
+    app.build(force_all=True)
+
+
+def test_text_documentation(tmp_path):
+    """Test Sphinx build of text documentation."""
+    source_dir = SOURCE_DIR
+    config_dir = CONFIG_DIR
+    output_dir = tmp_path
+    doctree_dir = tmp_path / "doctrees"
+    app = Sphinx(
+        source_dir,
+        config_dir,
+        output_dir,
+        doctree_dir,
+        buildername="text",
+        warningiserror=True,
+    )
+    app.build(force_all=True)

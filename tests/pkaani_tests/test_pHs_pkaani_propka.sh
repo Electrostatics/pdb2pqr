0f065d888ec04fa0cfb535076735474ff1580a18
skanda <sastrys1@gene.com>
2025-05-07 20:06:05 -0700

    added one more test PDB, created a testing script

--- tests/pkaani_tests/test_pHs_pkaani_propka.sh
+++ tests/pkaani_tests/test_pHs_pkaani_propka.sh
@@ -1,103 +1,98 @@
 #!/bin/bash
 
-# pkaani test code.
-# count number of residues at a protonation state before pdb2pqr at a pH of 5.0
-# All the Asp, Glu, and His residues should be protonated, none the LYS, ARG, and TYR
-# residues should be deprotonated. Ensuring that this is the case
+# Usage: ./test_pHs_pkaani_propka.sh [input_pdb]
 
-asps=$(cat 1brs.pdb | grep "CA  ASP" | wc -l)
-glus=$(cat 1brs.pdb | grep "CA  GLU" | wc -l)
-his_s=$(cat 1brs.pdb | grep "CA  HIS" | wc -l)
-lys=$(cat 1brs.pdb | grep "CA  LYS" | wc -l)
-args=$(cat 1brs.pdb | grep "CA  ARG" | wc -l)
-tyrs=$(cat 1brs.pdb | grep "CA  TYR" | wc -l)
-pdb2pqr30 --ffout AMBER --titration-state-method propka --with-ph 5.0 1brs.pdb 1brs_propka.pqr 
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 input.pdb"
+    exit 1
+fi
 
-ashs=$(cat 1brs_propka.pqr | grep "CA  ASH" | wc -l)
-glhs=$(cat 1brs_propka.pqr | grep "CA  GLH" | wc -l)
-hips=$(cat 1brs_propka.pqr | grep "CA  HIP" | wc -l)
-pqr_lys=$(cat 1brs_propka.pqr | grep "CA  LYS" | wc -l)
-pqr_arg=$(cat 1brs_propka.pqr | grep "CA  ARG" | wc -l)
-pqr_tyr=$(cat 1brs_propka.pqr | grep "CA  TYR" | wc -l)
+pdb_file="$1"
+base_name=$(basename "$pdb_file" .pdb)
 
+# count residues in input PDB
+# only works if there are no ANISOU records, only ATOM records in the PDB
+asps=$(grep "CA  ASP" "$pdb_file" | wc -l)
+glus=$(grep "CA  GLU" "$pdb_file" | wc -l)
+his_s=$(grep "CA  HIS" "$pdb_file" | wc -l)
+lys=$(grep "CA  LYS" "$pdb_file" | wc -l)
+args=$(grep "CA  ARG" "$pdb_file" | wc -l)
+tyrs=$(grep "CA  TYR" "$pdb_file" | wc -l)
 
-echo "PROPKA RESULTS"
+# Run PROPKA
+pdb2pqr30 --ffout AMBER --titration-state-method propka --with-ph 5.0 "$pdb_file" "${base_name}_propka.pqr"
+
+ashs=$(grep "CA  ASH" "${base_name}_propka.pqr" | wc -l)
+glhs=$(grep "CA  GLH" "${base_name}_propka.pqr" | wc -l)
+hips=$(grep "CA  HIP" "${base_name}_propka.pqr" | wc -l)
+pqr_lys=$(grep "CA  LYS" "${base_name}_propka.pqr" | wc -l)
+pqr_arg=$(grep "CA  ARG" "${base_name}_propka.pqr" | wc -l)
+pqr_tyr=$(grep "CA  TYR" "${base_name}_propka.pqr" | wc -l)
+
+echo "PROPKA RESULTS ${pdb_file}"
 echo "---------------------------------------"
-
 echo "Aspartates in pdb file: (none should be protonated after pdb2pqr)"
 echo $asps 
 echo "Protonated aspartates in pqr file:"
 echo $ashs
-
 echo "Glutamates in pdb file: (none should not be protonated after pdb2pqr)"
 echo $glus
 echo "Protonated glutamates in pqr file:"
 echo $glhs
-
 echo "Histidines in pdb file: (all should be protonated after pdb2pqr)"
 echo $his_s
 echo "Protonated histidines in pqr file:"
 echo $hips
-
 echo "protonated Lysines in pdb file: (all should be protonated after pdb2pqr)"
 echo $lys
 echo "Lysines in pqr file:"
 echo $pqr_lys
-
 echo "protonated Arginines in pdb file: (all should be protonated after pdb2pqr)"
 echo $args
 echo "Arginines in pqr file:"
 echo $pqr_arg
-
 echo "protonated Tyrosines in pdb file: (all should be protonated after pdb2pqr)"
 echo $tyrs
 echo "Tyrosines in pqr file:"
 echo $pqr_tyr
-
 echo "---------------------------------------"
 printf "\n"
 
-pdb2pqr30 --ffout AMBER --titration-state-method pkaani --with-ph 5.0 1brs.pdb 1brs_pkaani.pqr
+# Run PKAANI
+pdb2pqr30 --ffout AMBER --titration-state-method pkaani --with-ph 5.0 "$pdb_file" "${base_name}_pkaani.pqr"
 
-ashs=$(cat 1brs_pkaani.pqr | grep "CA  ASH" | wc -l)
-glhs=$(cat 1brs_pkaani.pqr | grep "CA  GLH" | wc -l)
-hips=$(cat 1brs_pkaani.pqr | grep "CA  HIP" | wc -l)
-pqr_lys=$(cat 1brs_pkaani.pqr | grep "CA  LYS" | wc -l)
-pqr_arg=$(cat 1brs_pkaani.pqr | grep "CA  ARG" | wc -l)
-pqr_tyr=$(cat 1brs_pkaani.pqr | grep "CA  TYR" | wc -l)
+ashs=$(grep "CA  ASH" "${base_name}_pkaani.pqr" | wc -l)
+glhs=$(grep "CA  GLH" "${base_name}_pkaani.pqr" | wc -l)
+hips=$(grep "CA  HIP" "${base_name}_pkaani.pqr" | wc -l)
+pqr_lys=$(grep "CA  LYS" "${base_name}_pkaani.pqr" | wc -l)
+pqr_arg=$(grep "CA  ARG" "${base_name}_pkaani.pqr" | wc -l)
+pqr_tyr=$(grep "CA  TYR" "${base_name}_pkaani.pqr" | wc -l)
 
-echo "PKAANI RESULTS"
+echo "PKAANI RESULTS ${pdb_file}"
 echo "---------------------------------------"
-
 echo "Aspartates in pdb file: (most should not be protonated after pdb2pqr)"
 echo $asps 
 echo "Protonated aspartates in pqr file:"
 echo $ashs
-
 echo "Glutamates in pdb file: (most should not be protonated after pdb2pqr)"
 echo $glus
 echo "Protonated glutamates in pqr file:"
 echo $glhs
-
 echo "Histidines in pdb file: (all should be protonated after pdb2pqr)"
 echo $his_s
 echo "Protonated histidines in pqr file:"
 echo $hips
-
 echo "protonated Lysines in pdb file: (none should be deprotonated after pdb2pqr)"
 echo $lys
 echo "Lysines in pqr file:"
 echo $pqr_lys
-
 echo "protonated Arginines in pdb file: (none should be deprotonated after pdb2pqr)"
 echo $args
 echo "Arginines in pqr file:"
 echo $pqr_arg
-
 echo "protonated Tyrosines in pdb file: (none should be deprotonated after pdb2pqr)"
 echo $tyrs
 echo "Tyrosines in pqr file:"
 echo $pqr_tyr
-
 echo "---------------------------------------"
 printf "\n"
